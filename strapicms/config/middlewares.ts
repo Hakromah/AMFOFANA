321317024f8c09f022556d5bc8ff0bb861c752fe
hassan <kkromah12@gmail.com>
2026-04-02 22:17:59 +0300

    back to strapi

--- strapicms/config/middlewares.ts
+++ strapicms/config/middlewares.ts
@@ -3,30 +3,11 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          // This allows the admin panel to show images from Cloudinary
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
-      origin: [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://192.168.1.137:3000',
-        'https://amfofana.vercel.app'
-      ],
+      origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://192.168.1.137:3000', 'https://amfofana.vercel.app'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
@@ -35,18 +16,7 @@ const config: Core.Config.Middlewares = [
   },
   'strapi::poweredBy',
   'strapi::query',
-  //'strapi::body',
-  {
-    name: 'strapi::body',
-    config: {
-      formLimit: '10mb',
-      jsonLimit: '10mb',
-      textLimit: '10mb',
-      formidable: {
-        maxFileSize: 10 * 1024 * 1024, // 10mb
-      },
-    },
-  },
+  'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
@@ -61,11 +31,30 @@ export default config;
 // const config: Core.Config.Middlewares = [
 //   'strapi::logger',
 //   'strapi::errors',
-//   'strapi::security',
+//   {
+//     name: 'strapi::security',
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           'connect-src': ["'self'", 'https:'],
+//           // This allows the admin panel to show images from Cloudinary
+//           'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+//           'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+//           upgradeInsecureRequests: null,
+//         },
+//       },
+//     },
+//   },
 //   {
 //     name: 'strapi::cors',
 //     config: {
-//       origin: ['http://localhost:3000', 'http://127.0.0.1:3000','http://192.168.1.137:3000','https://amfofana.vercel.app'],
+//       origin: [
+//         'http://localhost:3000',
+//         'http://127.0.0.1:3000',
+//         'http://192.168.1.137:3000',
+//         'https://amfofana.vercel.app'
+//       ],
 //       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
 //       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
 //       keepHeaderOnError: true,
@@ -74,7 +63,18 @@ export default config;
 //   },
 //   'strapi::poweredBy',
 //   'strapi::query',
-//   'strapi::body',
+//   //'strapi::body',
+//   {
+//     name: 'strapi::body',
+//     config: {
+//       formLimit: '10mb',
+//       jsonLimit: '10mb',
+//       textLimit: '10mb',
+//       formidable: {
+//         maxFileSize: 10 * 1024 * 1024, // 10mb
+//       },
+//     },
+//   },
 //   'strapi::session',
 //   'strapi::favicon',
 //   'strapi::public',
@@ -83,3 +83,4 @@ export default config;
 // ];
 
 // export default config;
+
